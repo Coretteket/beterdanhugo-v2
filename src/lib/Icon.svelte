6bb02f4fc237439ddb2330303119286b8af58d64
Quinten Coret <Qcoret@gmail.com>
2022-02-11 21:20:01 +0100

    Creating button icon component.

---
+++ src/lib/Icon.svelte
@@ -0,0 +1,26 @@
+<script>
+	import icons from '$lib/_icons';
+	export let name = '';
+	export let brand = false;
+	export let solid = false;
+	export let toggle = false;
+
+	let weight = brand ? 100 : solid ? 300 : 200;
+</script>
+
+<span class:toggle style:font-weight={weight}>{@html '&#x' + icons[name]}</span>
+
+<style>
+	span {
+		display: inline-block;
+		font-family: 'fa';
+		-moz-osx-font-smoothing: grayscale;
+		-webkit-font-smoothing: antialiased;
+		text-rendering: auto;
+		line-height: 1;
+		transition: transform 0.4s ease;
+	}
+	.toggle {
+		transform: var(--toggle);
+	}
+</style>
---
+++ src/lib/_icons.js
@@ -0,0 +1,26 @@
+let icons = {
+	github: 'f09b;',
+	twitter: 'f099;',
+	faq: 'f059',
+	sun: 'f185',
+	moon: 'f186',
+	back: 'f0a8',
+	theme: 'f042',
+	socdis: 'e068',
+	workhome: 'e065',
+	masks: 'e063',
+	events: 'f8cb',
+	theater: 'f630',
+	gather: 'f1fd',
+	clubs: 'f79f',
+	horeca: 'f2e7',
+	shops: 'f07a',
+	eduni: 'f501',
+	edmid: 'f51c',
+	edlow: 'f1ae',
+	lockdown: 'f30d',
+	curfew: 'f34e',
+	borders: 'f072'
+};
+
+export default icons;
