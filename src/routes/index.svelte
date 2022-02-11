f5b87dc1744cbd992521b63b7fd096b088631824
Quinten Coret <Qcoret@gmail.com>
2022-02-11 21:23:23 +0100

    Creating simple Index page.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,20 @@
-<h1>Hello world!</h1>
+<p>
+	Na twee jaar persconferenties heeft iedereen eigen ideeën over het coronabeleid. Maar kunnen we
+	het echt beter dan Hugo de Jonge? In dit spel kom jij voor dezelfde keuzes te staan, maar mag je
+	je eigen weg door de pandemie kiezen.
+</p>
+<p>
+	Voer maatregelen in en hef ze weer op om de coronacijfers te zien veranderen, en hou het nieuws in
+	de gaten om te kijken hoe jouw beleid in de samenleving valt. Krijg jij corona onder controle?
+</p>
+
+<a href="/game">Start het spel</a>
+
+<style>
+	p {
+		line-height: 1.1;
+		max-width: 600px;
+		margin-top: 0;
+		margin-bottom: 0.5em;
+	}
+</style>
