da53ce6fb93f28997cef586af3acf60f0540a013
Quinten Coret <Qcoret@gmail.com>
2022-02-11 21:24:04 +0100

    Creating grid for Game page.

---
+++ src/routes/game.svelte
@@ -0,0 +1,63 @@
+<script>
+	import Time from '$lib/Time.svelte';
+	import MeasureList from '$lib/MeasureList.svelte';
+</script>
+
+<div class="container">
+	<div class="side">News</div>
+	<div class="time"><Time /></div>
+	{#each [...Array(5).keys()] as id}
+		<div class={'btn' + id}><MeasureList {id} /></div>
+	{/each}
+</div>
+
+<style lang="scss">
+	.container {
+		display: grid;
+		grid-auto-columns: 1fr;
+		gap: 0 1em;
+		grid-auto-flow: row;
+		grid-template-columns: minmax(180px, 1fr) minmax(180px, 1fr) 350px;
+		grid-template-rows: repeat(4, auto);
+		grid-template-areas:
+			'time time side'
+			'btn0 btn3 side'
+			'btn1 btn4 side'
+			'btn2 btn5 side';
+
+		@media (max-width: 1000px) {
+			grid-template-columns: 350px 350px;
+			grid-template-rows: repeat(7, auto);
+			grid-template-areas:
+				'time side'
+				'btn0 side'
+				'btn1 side'
+				'btn2 side'
+				'btn3 side'
+				'btn4 side'
+				'btn5 side';
+		}
+
+		@media (max-width: 765px) {
+			grid-template-columns: 1fr;
+			grid-template-rows: repeat(7, auto);
+			grid-template-areas:
+				'side'
+				'time'
+				'btn0'
+				'btn1'
+				'btn2'
+				'btn3'
+				'btn4'
+				'btn5';
+		}
+	}
+
+	$list: 'side', 'time', 'btn0', 'btn1', 'btn2', 'btn3', 'btn4', 'btn5';
+
+	@each $i in $list {
+		.#{$i} {
+			grid-area: #{$i};
+		}
+	}
+</style>
