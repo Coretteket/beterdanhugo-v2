8bf0cbbbbd7b01f2a81e0bd9c9f9ac1d4d514a09
Quinten Coret <Qcoret@gmail.com>
2022-02-14 23:19:33 +0100

    Initial Chartist config.

---
+++ src/lib/chart.js
@@ -0,0 +1,28 @@
+export const options = {
+	axisX: {
+		offset: 0,
+		showLabel: false,
+		showGrid: false
+	},
+	axisY: {
+		offset: 35,
+		showLabel: true,
+		scaleMinSpace: 10
+	},
+	showPoint: false,
+	showArea: true,
+	low: 0,
+	chartPadding: {
+		top: 0,
+		right: 0,
+		bottom: 0,
+		left: 0
+	},
+	fullWidth: true,
+	chartPadding: {
+		top: 15,
+		right: 5,
+		bottom: 7,
+		left: 5
+	}
+};
