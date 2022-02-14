e9c4eb8a69990036e7a57152e331d880054af960
Quinten Coret <Qcoret@gmail.com>
2022-02-14 23:19:53 +0100

    Creating styled Box component.

---
+++ src/lib/Box.svelte
@@ -0,0 +1,16 @@
+<div><slot /></div>
+
+<style>
+	div {
+		border: 2px solid var(--border);
+		line-height: 1.1;
+		background: var(--box);
+		margin-top: 0.4em;
+		margin-bottom: 0.8em;
+		padding: 0.6em;
+		border-radius: 0.6em;
+		text-align: left;
+		max-height: 50em;
+		box-shadow: var(--shadow);
+	}
+</style>
